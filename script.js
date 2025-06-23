d5a3e4a60628a1435214dc6ec0e3d851135c7eec
Aketchy <aketchyouma@gmail.com>
2025-06-23 19:50:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -31,3 +31,8 @@ if (form) {
     }
     });
 }
+
+document.getElementById("menu-toggle").addEventListener("click", function () {
+  document.getElementById("nav-links").classList.toggle("active");
+});
+```
